3b8d32bc1d8fe44e76594e24c68fcfbb827516e7
Federico Aloi <federico.aloi@gmail.com>
2018-05-16 17:49:31 -0300

    PoblacionPorPais carga los datos con binding

--- app/src/main/java/ar/edu/unq/cpi/geography/view/population/PoblacionPorPaisActivity.java
+++ app/src/main/java/ar/edu/unq/cpi/geography/view/population/PoblacionPorPaisActivity.java
@@ -1,66 +1,41 @@
 package ar.edu.unq.cpi.geography.view.population;
 
-import android.support.v7.app.AppCompatActivity;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
 import ar.edu.unq.cpi.geography.R;
-import ar.edu.unq.cpi.geography.data.restricted.FixedRestrictedCountryDataProvider;
-import ar.edu.unq.cpi.geography.data.restricted.RestrictedCountryData;
-import ar.edu.unq.cpi.geography.data.restricted.RestrictedCountryDataProvider;
-import ar.edu.unq.cpi.geography.data.worldbank.WorldBankDataProvider;
+import ar.edu.unq.cpi.geography.databinding.ActivityPoblacionPorPaisBinding;
 
+/**
+ * Esta es una version de PoblacionPorPaisActivity que incluye un controller.
+ * En esta version, el controller habla con el DataProvider.s
+ */
 public class PoblacionPorPaisActivity extends AppCompatActivity {
 
-    private TextView nombrePais;
-    private TextView poblacionPais;
+    private PoblacionPorPaisController controller = new PoblacionPorPaisController();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        this.setContentView(R.layout.activity_poblacion_por_pais);
 
-        this.nombrePais = this.findViewById(R.id.poblacionPorPais_pais);
-        this.poblacionPais = this.findViewById(R.id.poblacionPorPais_poblacion);
+        ActivityPoblacionPorPaisBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_poblacion_por_pais);
+        binding.setController(controller);
 
-        Button elegirArgentina = this.findViewById(R.id.poblacionPorPais_argentina);
+        Button elegirArgentina = this.findViewById(R.id.argentina);
         elegirArgentina.setOnClickListener((View v) -> {
-            this.showDataDePais_fetch("Argentina");
+            this.controller.setCountryName("Argentina");
         });
-        Button elegirBrasil = this.findViewById(R.id.poblacionPorPais_brasil);
+        Button elegirBrasil = this.findViewById(R.id.brasil);
         elegirBrasil.setOnClickListener((View v) -> {
-            this.showDataDePais_fetch("Brasil");
+            this.controller.setCountryName("Brasil");
         });
-        Button elegirParaguay = this.findViewById(R.id.poblacionPorPais_paraguay);
+        Button elegirParaguay = this.findViewById(R.id.paraguay);
         elegirParaguay.setOnClickListener((View v) -> {
-            this.showDataDePais_fetch("Paraguay");
+            this.controller.setCountryName("Paraguay");
         });
-    }
-
-    public void showDataDePais_get(String nombreDePais) {
-        RestrictedCountryData data = new FixedRestrictedCountryDataProvider().getCountryData(nombreDePais);
-        this.nombrePais.setText(nombreDePais);
-        this.poblacionPais.setText(String.valueOf(data.getPopulation()));
-    }
-
-    public void showDataDePais_fetch(String nombreDePais) {
-        RestrictedCountryDataProvider.api().fetchCountryData(nombreDePais,
-                // este es el callback, o continuation,
-                // mediante el callback le digo al DataProvider qué quiero que haga
-                // cuando obtenga los datos
-                // en el parámetro (data) me llegan los datos que obtuvo el DataProvider
-                // este es un ejemplo de comunicación *asincrónica*
-                // entre la Activity y el DataProvider
-                (data) -> {
-                    Log.d("PoblacionPorPais", "adentro del bloque");
-                    this.nombrePais.setText(nombreDePais);
-                    this.poblacionPais.setText(String.valueOf(data.getPopulation()));
-                }
-        );
-        Log.d("PoblacionPorPais", "afuera del bloque");
     }
 
 }
--- app/src/main/java/ar/edu/unq/cpi/geography/view/population/PoblacionPorPaisController.java
+++ app/src/main/java/ar/edu/unq/cpi/geography/view/population/PoblacionPorPaisController.java
@@ -1,31 +1,39 @@
 package ar.edu.unq.cpi.geography.view.population;
 
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import ar.edu.unq.cpi.geography.BR;
 import ar.edu.unq.cpi.geography.R;
 import ar.edu.unq.cpi.geography.data.restricted.FixedRestrictedCountryDataProvider;
 import ar.edu.unq.cpi.geography.data.restricted.RestrictedCountryData;
 import ar.edu.unq.cpi.geography.data.restricted.RestrictedCountryDataProvider;
 
-public class PoblacionPorPaisController {
+public class PoblacionPorPaisController extends BaseObservable {
 
     // RestrictedCountryData es el DataObject
     private RestrictedCountryData currentCountryData = RestrictedCountryData.noData();
 
     public void setCountryName(String nombreDePais) {
         // FixedRestrictedCountryDataProvider es el DataProvider
-        this.currentCountryData = new FixedRestrictedCountryDataProvider()
-                .getCountryData(nombreDePais);
+        this.currentCountryData = new FixedRestrictedCountryDataProvider().getCountryData(nombreDePais);
+        notifyPropertyChanged(BR.countryName);
+        notifyPropertyChanged(BR.population);
     }
 
     public boolean hasData() { return this.currentCountryData.hasData(); }
+
+    @Bindable
     public String getCountryName() {
         return this.hasData() ? this.currentCountryData.getApiName() : "";
     }
+
+    @Bindable
     public String getPopulation() {
         return this.hasData() ? String.valueOf(this.currentCountryData.getPopulation()) : "";
     }
