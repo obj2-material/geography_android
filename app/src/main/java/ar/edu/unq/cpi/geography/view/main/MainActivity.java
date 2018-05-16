d4e9580b68961dfacd618d75824fc9f702abb1cc
Federico Aloi <federico.aloi@gmail.com>
2018-05-16 16:59:39 -0300

    Bindeados botones "Ir a..." de MainActivity

--- app/src/main/java/ar/edu/unq/cpi/geography/view/main/MainActivity.java
+++ app/src/main/java/ar/edu/unq/cpi/geography/view/main/MainActivity.java
@@ -1,17 +1,12 @@
 package ar.edu.unq.cpi.geography.view.main;
 
-import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
 
 import ar.edu.unq.cpi.geography.R;
 import ar.edu.unq.cpi.geography.databinding.MainActivityBinding;
 import ar.edu.unq.cpi.geography.tools.ApplicationToolset;
-import ar.edu.unq.cpi.geography.view.population.ListaDePaisesActivity;
-import ar.edu.unq.cpi.geography.view.population.PoblacionPorPaisActivity;
 
 /**
  * Ventana inicial de la aplicacion.
@@ -27,41 +22,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         controller = new MainActivityControlller(getString(R.string.saludoInicial));
         MainActivityBinding binding = DataBindingUtil.setContentView(this, R.layout.main_activity);
         binding.setController(controller);
 
         ApplicationToolset.setContext(this);
-
-        /*
-         Pantalla que muestra la poblacion de Argentina, Brasil o Paraguay
-         Usa RestrictedCountryData, package ar.edu.unq.cpi.geography.data.restricted,
-         info de http://api.population.io
-         */
-        Button botonIrAPoblacion = this.findViewById(R.id.irAPoblacionPorPais);
-        botonIrAPoblacion.setOnClickListener((View v) -> {
-            this.irAPoblacionPorPais();
-        });
-
-        /*
-         Pantalla que muestra la lista de paises de America del Sud, eligiendo un pais
-         se accede a poblacion y superficie del pais.
-         Usa RestrictedCountryData, package ar.edu.unq.cpi.geography.data.expanded,
-         info de https://restcountries.eu
-         */
-        Button botonIrAListaDePaises = this.findViewById(R.id.irAListaDePaises);
-        botonIrAListaDePaises.setOnClickListener((View v) -> {
-            this.irAListaDePaises();
-        });
     }
 
-    public void irAPoblacionPorPais() {
-        Intent intent = new Intent(this, PoblacionPorPaisActivity.class);
-        startActivity(intent);
-    }
-
-    public void irAListaDePaises() {
-        Intent intent = new Intent(this, ListaDePaisesActivity.class);
-        startActivity(intent);
-    }
 }
--- app/src/main/java/ar/edu/unq/cpi/geography/view/main/MainActivityControlller.java
+++ app/src/main/java/ar/edu/unq/cpi/geography/view/main/MainActivityControlller.java
@@ -1,11 +1,14 @@
 package ar.edu.unq.cpi.geography.view.main;
 
+import android.content.Intent;
 import android.databinding.BaseObservable;
 import android.databinding.Bindable;
 import android.view.View;
 
 import ar.edu.unq.cpi.geography.BR;
 import ar.edu.unq.cpi.geography.R;
+import ar.edu.unq.cpi.geography.view.population.ListaDePaisesActivity;
+import ar.edu.unq.cpi.geography.view.population.PoblacionPorPaisActivity;
 
 public class MainActivityControlller extends BaseObservable {
     private String saludo;
@@ -26,5 +29,29 @@ public class MainActivityControlller extends BaseObservable {
 
     public void cambiarSaludo(View view) {
         this.setSaludo(view.getResources().getString(R.string.saludoFinal));
+    }
+
+    /*
+    Pantalla que muestra la poblacion de Argentina, Brasil o Paraguay
+    Usa RestrictedCountryData, package ar.edu.unq.cpi.geography.data.restricted,
+    info de http://api.population.io
+    */
+    public void irAPoblacionPorPais(View view) {
+        abrirActivity(view, PoblacionPorPaisActivity.class);
+    }
+
+    /*
+     Pantalla que muestra la lista de paises de America del Sud, eligiendo un pais
+     se accede a poblacion y superficie del pais.
+     Usa RestrictedCountryData, package ar.edu.unq.cpi.geography.data.expanded,
+     info de https://restcountries.eu
+     */
+    public void irAListaDePaises(View view) {
+        abrirActivity(view, ListaDePaisesActivity.class);
+    }
+
+    private void abrirActivity(View view, Class<?> activityClass) {
+        Intent intent = new Intent(view.getContext(), activityClass);
+        view.getContext().startActivity(intent);
     }
 }
